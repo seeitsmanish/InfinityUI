4dde73d1a7e10a3a8221277fbd8ce24cbc920f93
See-Its-Manish <seeitsmanish@gmail.com>
2024-11-19 23:11:43 +0530

    feat: i-avatar component with lazy loading

---
+++ src/components/i-avatar/i-avatar.tsx
@@ -0,0 +1,59 @@
+import { Component, Element, h, Prop, State } from "@stencil/core";
+import { ColorType, sizeType } from "../../models";
+
+@Component({
+    tag: 'i-avatar',
+    styleUrl: 'i-avatar.scss',
+    shadow: false,
+})
+
+export class IAvatar {
+
+    @Prop() src: string = ''; // Image source
+    @Prop() alt: string = 'avatar image'; // Alt text for the image
+    @Prop() lazy?: boolean = true; // Lazy loading for the image
+
+    @Prop() color?: ColorType = 'default';
+    @Prop() size: sizeType = 'sm'; // Size of the avatar
+    @Prop() shape: 'circular' | 'square' | 'rounded-square' = 'circular'; // Shape of the avatar
+
+    @Prop() ring?: boolean = false; // Whether the avatar has a ring
+    @Prop() ringColor?: string; // Color of the ring
+    @Prop() ringWidth?: number; // Width of the ring
+
+    @Prop() classes: string = ''; // Custom classes for styling
+
+    @Element() el: HTMLElement;
+    @State() isAvatarVisible: boolean = false;
+    componentDidLoad() {
+        const observer = new IntersectionObserver((entries) => {
+            const avatarElement = entries[0];
+            if (avatarElement.isIntersecting) {
+                this.isAvatarVisible = true;
+                observer.disconnect();
+            }
+        })
+        observer.observe(this.el);
+    }
+
+    getInitials(str: string): string {
+        const words = str.trim().split(' ');
+        return words.map(word => word[0]).join('').toUpperCase();
+    }
+
+    render() {
+        console.log(this.isAvatarVisible);
+        const initials = this.getInitials(this.alt);
+        const avatarClassName = `i-avatar-root ${this.size} ${this.shape} ${this.color} ${this.classes}`
+        return (
+            <div tabindex='0' class={avatarClassName}>
+                {
+                    this.isAvatarVisible ?
+                        <img class='i-avatar-image' loading='lazy' src={this.src} alt={this.alt} onError={() => this.isAvatarVisible = false} />
+                        :
+                        <span class='i-avatar-initials'>{initials}</span>
+                }
+            </div>
+        )
+    }
+}
