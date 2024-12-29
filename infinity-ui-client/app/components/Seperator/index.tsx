5e1369566500ed8d69b4d3f14c4831bc378f90ed
See-Its-Manish <seeitsmanish@gmail.com>
2024-12-29 23:04:22 +0530

    feat: Seperator

--- infinity-ui-client/app/components/Seperator/index.tsx
+++ infinity-ui-client/app/components/Seperator/index.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 const Seperator = ({ className }: { className?: string; }) => {
     return (
-        <div className={cn('w-full h-[1px] bg-slate-400 rounded-lg dark:bg-slate-700', className)}>
+        <div className={cn('w-full h-[1px] my-5 bg-slate-400 rounded-lg dark:bg-slate-700', className)}>
         </div>
     )
 }
