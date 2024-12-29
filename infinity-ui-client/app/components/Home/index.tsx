3c8819dacac7276338a2c6668bee64818a32563a
See-Its-Manish <seeitsmanish@gmail.com>
2024-12-29 23:20:19 +0530

    chore: Added Link to CTA Buttons

--- infinity-ui-client/app/components/Home/index.tsx
+++ infinity-ui-client/app/components/Home/index.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image"
 import { Spotlight } from "../ui/Spotlight"
 import { ArrowRight, Copy } from "lucide-react"
 import CopyItem from "../shared/CopyItem"
 import Navbar from "../Navbar"
+import Link from "next/link"
 
 type HomePropsType = {}
 
@@ -46,7 +46,7 @@ const Home = ({ }: HomePropsType) => {
                     {/* CTA Buttons */}
                     <div className="flex flex-col sm:flex-row items-center gap-4 md:gap-8">
                         {/* Explore Components */}
-                        <button className="relative inline-flex h-12 w-full sm:w-auto overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                        <Link href="/docs" className="relative inline-flex h-12 w-full sm:w-auto overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
                                 <div className="flex gap-2 items-center group w-[280px] justify-center">
@@ -56,7 +56,7 @@ const Home = ({ }: HomePropsType) => {
                                     </div>
                                 </div>
                             </span>
-                        </button>
+                        </Link>
 
                         {/* Copy Button */}
                         <div className="inline-flex h-12 w-full sm:w-auto animate-shimmer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 sm:px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
